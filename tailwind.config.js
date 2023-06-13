b7ce55290ada6a6706d5e042218ed9d2129b3191
dialite <raymondolisa01@gmail.com>
2023-06-13 16:27:04 +0100

    Add satoshi and inter fontFamily in tailwind.config.js and initialise it in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
