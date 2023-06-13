9fe206361b2931cc8e04e86a8bead7601a56f61a
dialite <raymondolisa01@gmail.com>
2023-06-13 17:25:20 +0100

    Add image and initialize header and nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@ import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center items-center flex-col"></header>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-center items-center flex-col">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+      </nav>
+    </header>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ["./src/**/*.{html,js,jsx}"],
-  // darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
