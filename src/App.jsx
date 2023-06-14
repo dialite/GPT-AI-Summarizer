7fa90e28742195b05152cb619659c06cbf6219f6
dialite <raymondolisa01@gmail.com>
2023-06-14 12:33:29 +0100

    Create nav content and apply tailwindcss style

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <div className="main">
         <div className="gradient" />
       </div>
-      <div>
+      <div className="app">
         <Hero />
         <Demo />
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,18 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/dialite");
+          }}
+          className="black_btn"
+        >
+          GitHub
+        </button>
       </nav>
     </header>
   );
