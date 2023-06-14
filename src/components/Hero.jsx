1d3aeefa4b49ac909f53b694f39c0f12c33216dd
dialite <raymondolisa01@gmail.com>
2023-06-14 12:36:37 +0100

    Create title for header section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,6 +16,11 @@ const Hero = () => {
           GitHub
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
     </header>
   );
 };
